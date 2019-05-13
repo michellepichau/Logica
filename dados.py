3aca1168ed2abc7e35cecd2de1bc750c04874a8b
michellepichau <50593955+michellepichau@users.noreply.github.com>
2019-05-13 15:29:28 -0300

    dados:1ª versão

---
+++ dados.py
@@ -0,0 +1,14 @@
+.from random import randint
+
+print("Jogo de Dados:")
+
+dados1 = randint(1, 6)
+dados2 = randint(1, 6)
+dados3 = randint(1, 6)
+dados4 = randint(1, 6)
+
+print("Dados 1:", dados1)
+print("Dados 2:", dados2)
+print("Dados 3:", dados3)
+print("Dados 4:", dados4)
+programaçao 
